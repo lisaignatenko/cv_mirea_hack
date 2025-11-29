a600bc431274bf472f6f670eee5a08c4f4839aaa
Никита Сорокин <nikitasorokin@MacBook-Air-Nikita-5.local>
2025-11-29 23:22:41 +0300

    just parse it somehow

--- cv/src/cv/api/routes/segment.py
+++ cv/src/cv/api/routes/segment.py
@@ -1,4 +1,5 @@
-from typing import Any
+from typing import Any, Dict, Optional
+import json
 
 from fastapi import APIRouter
 
@@ -7,6 +8,14 @@ from cv.api.schemas import RequestPayload
 router = APIRouter(tags=["segment"])
 
 
+def get_jsonl_line(file_path, line_index) -> Optional[Dict]:
+    with open(file_path, 'r', encoding='utf-8') as file:
+        for current_line, line in enumerate(file):
+            if current_line == line_index:
+                return json.loads(line.strip())
+    return None
+
 @router.post("/segment")
 async def segment(payload: RequestPayload) -> dict[str, Any]:
-    return ВАША_ФУНКЦИЯ(payload.id)
+    # TODO figure out path to data.jsonl
+    return get_jsonl_line('data.jsonl', payload.id)
